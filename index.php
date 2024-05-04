bb58cc3b75efc5d2c7546b9fae9c4ff24982f37f
deyvidfernandes <deyvid.guedes.fer@gmail.com>
2024-05-04 15:28:13 -0300

    Adição da página inicial para usuários cadastrados

--- index.php
+++ index.php
@@ -40,12 +40,12 @@
                </div>
                <p id="hero_paragraph">Aqui você encontra um método de aprendizagem semelhante a Flashcards, mas preparado com um catálogo de mais de 200 verbos irregulares e muitos outros recursos para facilitar seu aprendizado</p>
                <div id="hero_buttons-container">
-                  <button class="action-button --limited">Login</button>
-                  <button class="action-button --limited">Cadastre-se</button>
+                  <a class="action-button --limited" href="./login.html" >Login</a>
+                  <a class="action-button --limited" href="./signup.html">Cadastre-se</a>
                </div>
             </div>
          </header>
-         <main>
+         <main class="bottom-padding">
             <section class="test-container">
                <h2 class="light-title">Experimente nosso método</h2>
                <button class="action-button --limited">Vamos lá!</button>
@@ -109,8 +109,59 @@
    </body>
    EOD;
 
+   $privateHomePage = <<<EOD
+   <body>
+   <nav>
+      <a href="index.php">
+         <img src="./static/logo-master-of-tenses.png" height="70px" alt="Logo da Master of Tenses, um relógio branco vestindo uma faixa vermelha como um ninja, a seu lado, o nome da marca.">
+      </a>
+
+      <ul id="nav-links">
+         <li><a href="index.php">Início</a></li>
+         <li><a href="login.html">Buscar verbos</a></li>
+         <li><a href="signup.html">Meu aprendizado</a></li>
+         <li><a href="signup.html">Conta</a></li>
+      </ul>
+   </nav>
+      <header class="hero-container --logged">
+
+         <div id="welcome_back_container">
+            <div id="welcome_back-title_container">
+               <p>Olá de novo, Deyvid!</p>
+               <div>
+                  <p id="revision">Há 5 verbos para revisão hoje</p>
+                  <p id="recomendation">Recomendamos que você adicione 3 verbos a sua lista de aprendizado hoje</p>
+               </div>
+            </div>
+            <div id="hero_buttons-container">
+               <button class="action-button">Revisar</button>
+               <button class="action-button">Descobrir novos verbos</button>
+            </div>
+         </div>
+      </header>
+      <main>
+         <section class="test-container">
+            <h2 class="light-title">Explorar catálogo de verbos</h2>
+            <button class="action-button --limited">Vamos lá!</button>
+         </section>
+      </main>
+      <footer>
+         <div id="footer-links-container">
+            <p id="footer-internal-links">Master of tenses</p>
+            <a href="https://github.com/deyvidfernandes/MasterOfTenses" target="_blank" id="github-link"><i class="fa-brands fa-github" style="color: #ffffff; font-size: 48px;"></i></a>
+         </div>
+         <div id="footer-message">
+            <p>Desenvolvido com <i class="fa-solid fa-heart" style="color: #ffffff;"></i> para o TCC do curso Técnico em Desenvolvimento de Sistemas na ETEC</p>
+            <p>2024</p>
+         </div>
+         <!-- Posiciona o link do github ao lado dos links internos sem influenciar no seu posicionamento -->
+         <script src="./src/styling/footer.js"></script>
+      </footer>
+   </body>
+   EOD;
+
    if ($isLogged) {
-      echo "Você está logado";
+      echo $privateHomePage;
    } else {
       echo $publicHomePage;
    }
